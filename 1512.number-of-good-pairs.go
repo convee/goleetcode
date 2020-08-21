96d75fa7296d5d6623e15cc4e990124fec61484d
convee <convee.cn@gmail.com>
2020-08-22 01:17:45 +0800

    numIdenticalPairs2

--- 1512.number-of-good-pairs.go
+++ 1512.number-of-good-pairs.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//1512. 好数对的数目
 //https://leetcode-cn.com/problems/number-of-good-pairs/
 
 func numIdenticalPairs(nums []int) int {
@@ -23,7 +24,23 @@ func numIdenticalPairs(nums []int) int {
 	return n
 }
 
+func numIdenticalPairs2(nums []int) int {
+	res := 0
+
+	var m map[int]int = make(map[int]int, 10)
+	for i := 0; i < len(nums); i++ {
+		if v, ok := m[nums[i]]; ok {
+			res += v
+			m[nums[i]] = v + 1
+		} else {
+			m[nums[i]] = 1
+		}
+
+	}
+	return res
+}
+
 func main() {
 	nums := []int{1, 2, 3, 1, 1, 3}
-	fmt.Println(numIdenticalPairs(nums))
+	fmt.Println(numIdenticalPairs2(nums))
 }

dd83e67bdc229a843274f38e706a45e7ca7d706d
convee <convee.cn@gmail.com>
2020-08-24 00:04:05 +0800

    reverseLeftWords

--- offer/58.reverse-left-words.go
+++ offer/58.reverse-left-words.go
@@ -8,6 +8,19 @@ func reverseLeftWords(s string, n int) string {
 	return string(s[n:] + s[:n])
 }
 
+func reverseLeftWords2(s string, n int) string {
+	var str string
+	var str2 string
+	for i := 0; i < len(s); i++ {
+		if i < n {
+			str += string(s[i])
+		} else {
+			str2 += string(s[i])
+		}
+	}
+	return str2 + str
+}
+
 func main() {
-	fmt.Println(reverseLeftWords("abcdefg", 2))
+	fmt.Println(reverseLeftWords2("abcdefg", 2))
 }

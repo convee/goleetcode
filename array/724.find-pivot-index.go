7dcc7679b69749051fc4df8f06d627ea085e1883
convee <convee.cn@gmail.com>
2020-08-30 22:31:08 +0800

    寻找数组的中心索引

--- array/724.find-pivot-index.go
+++ array/724.find-pivot-index.go
@@ -25,6 +25,22 @@ func pivotIndex(nums []int) int {
 	return -1
 }
 
+func pivotIndex2(nums []int) int {
+	var sum, leftSum = 0, 0
+
+	for _, v := range nums {
+		sum += v
+	}
+
+	for j := 0; j < len(nums); j++ {
+		if leftSum == (sum - leftSum - nums[j]) {
+			return j
+		}
+		leftSum += nums[j]
+	}
+
+	return -1
+}
 func main() {
 	nums := []int{-1, -1, -1, 0, 1, 1}
 

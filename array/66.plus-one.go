e87298a274a8ce601cc5af48ebce1dc151daa6a6
convee <convee.cn@gmail.com>
2020-09-01 22:55:51 +0800

    plus-one

---
+++ array/66.plus-one.go
@@ -0,0 +1,24 @@
+package main
+
+//66. 加一
+//输入: [1,2,3]
+//输出: [1,2,4]
+//解释: 输入数组表示数字 123。
+//https://leetcode-cn.com/problems/plus-one/
+func plusOne(digits []int) []int {
+	l := len(digits)
+
+	if l == 0 {
+		return []int{1}
+	}
+
+	for i := l - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			return digits
+		} else {
+			digits[i] = 0
+		}
+	}
+	return append([]int{1}, digits...)
+}

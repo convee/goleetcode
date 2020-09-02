13ccbd3084b51896b48b5910a1c71c3ce77e154f
convee <convee.cn@gmail.com>
2020-09-02 22:22:33 +0800

    二分查找

---
+++ binary-search/704.binary-search.go
@@ -0,0 +1,18 @@
+package main
+
+//704. 二分查找
+//https://leetcode-cn.com/problems/binary-search/
+func search(nums []int, target int) int {
+	l, r, mid := 0, len(nums)-1, (len(nums)-1)/2
+	for r >= l {
+		if target == nums[mid] {
+			return mid
+		} else if target > nums[mid] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+		mid = (l + r) / 2
+	}
+	return -1
+}

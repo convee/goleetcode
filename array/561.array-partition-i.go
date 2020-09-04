7ee63532ff6b3fb8fd986c6515132f28ad4e2dae
convee <convee.cn@gmail.com>
2020-09-04 23:10:49 +0800

    数组拆分

---
+++ array/561.array-partition-i.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//561. 数组拆分 I
+//把从a1到an数组下标为奇数的数都加起来
+//https://leetcode-cn.com/problems/array-partition-i/
+func arrayPairSum(nums []int) int {
+	l := len(nums)
+	sum := 0
+	sort.Ints(nums)
+	for i := 0; i < l; i++ {
+		if i%2 == 0 {
+			sum += nums[i]
+		}
+	}
+	return sum
+}
+
+func main() {
+	nums := []int{2, 3, 4, 1}
+	fmt.Println(arrayPairSum(nums))
+}

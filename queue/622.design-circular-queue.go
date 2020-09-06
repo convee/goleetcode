3439e05691b121df57ae87b7d0ed88950c81e27a
convee <convee.cn@gmail.com>
2020-09-06 23:49:46 +0800

    最小栈

--- queue/622.design-circular-queue.go
+++ queue/622.design-circular-queue.go
@@ -2,6 +2,13 @@ package queue
 
 //622. 设计循环队列
 //https://leetcode-cn.com/problems/design-circular-queue/
+//MyCircularQueue(k): 构造器，设置队列长度为 k 。
+//Front: 从队首获取元素。如果队列为空，返回 -1 。
+//Rear: 获取队尾元素。如果队列为空，返回 -1 。
+//enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+//deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+//isEmpty(): 检查循环队列是否为空。
+//isFull(): 检查循环队列是否已满
 
 type MyCircularQueue struct {
 	data        []int

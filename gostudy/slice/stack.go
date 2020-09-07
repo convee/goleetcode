be4ab1fb4d71ae9a957f9919cdc378aa0bb7b8fe
convee <convee.cn@gmail.com>
2020-09-07 22:11:23 +0800

    go-slice-stack

---
+++ gostudy/slice/stack.go
@@ -0,0 +1,13 @@
+package slice
+
+func main() {
+	//创建栈
+	stack := make([]int, 0)
+	//push 押入
+	stack = append(stack, 10)
+	//pop 弹出
+	v := stack[len(stack)-1]
+	stack = stack[:len(stack)-1]
+	//检查栈空
+	len(stack) == 0
+}

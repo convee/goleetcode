a68bc24132d3c163eece4f3db37fcabd36948972
convee <convee.cn@gmail.com>
2020-09-09 22:17:24 +0800

    subsets

---
+++ gostudy/subsets.go
@@ -0,0 +1,29 @@
+package gostudy
+
+//给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+//思路：这是一个典型的应用回溯法的题目，简单来说就是穷尽所有可能性，算法模板如下
+func subsets(nums []int) [][]int {
+	// 保存最终结果
+	result := make([][]int, 0)
+	// 保存中间结果
+	list := make([]int, 0)
+	backtrack(nums, 0, list, &result)
+	return result
+}
+
+// nums 给定的集合
+// pos 下次添加到集合中的元素位置索引
+// list 临时结果集合(每次需要复制保存)
+// result 最终结果
+func backtrack(nums []int, pos int, list []int, result *[][]int) {
+	// 把临时结果复制出来保存到最终结果
+	ans := make([]int, len(list))
+	copy(ans, list)
+	*result = append(*result, ans)
+	// 选择、处理结果、再撤销选择
+	for i := pos; i < len(nums); i++ {
+		list = append(list, nums[i])
+		backtrack(nums, i+1, list, result)
+		list = list[0 : len(list)-1]
+	}
+}

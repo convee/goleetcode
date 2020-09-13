3bf8933b0689410716229a7e9252a5c4a51684ad
convee <convee.cn@gmail.com>
2020-09-13 22:47:56 +0800

    二叉树遍历

--- binary-tree/binary-tree.go
+++ binary-tree/binary-tree.go
@@ -8,6 +8,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//前序递归
 func preorderTraversal(root *TreeNode) {
 	if root == nil {
 		return
@@ -16,6 +17,58 @@ func preorderTraversal(root *TreeNode) {
 	fmt.Println(root.Val)
 	preorderTraversal(root.Left)
 	preorderTraversal(root.Right)
+}
+
+// V3：前序非递归遍历
+func preorderTraversal(root *TreeNode) []int {
+	// 非递归
+	if root == nil {
+		return nil
+	}
+	result := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+
+	for root != nil || len(stack) != 0 {
+		for root != nil {
+			// 前序遍历，所以先保存结果
+			result = append(result, root.Val)
+			stack = append(stack, root)
+			root = root.Left
+		}
+		// pop
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		root = node.Right
+	}
+	return result
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	// 通过lastVisit标识右子节点是否已经弹出
+	if root == nil {
+		return nil
+	}
+	result := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	var lastVisit *TreeNode
+	for root != nil || len(stack) != 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		// 这里先看看，先不弹出
+		node := stack[len(stack)-1]
+		// 根节点必须在右节点弹出之后，再弹出
+		if node.Right == nil || node.Right == lastVisit {
+			stack = stack[:len(stack)-1] // pop
+			result = append(result, node.Val)
+			// 标记当前这个节点已经弹出过
+			lastVisit = node
+		} else {
+			root = node.Right
+		}
+	}
+	return result
 }
 
 func main() {

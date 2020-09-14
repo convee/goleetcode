c48d785b714bfbafad932b973a31f19601d71232
convee <convee.cn@gmail.com>
2020-09-14 23:02:39 +0800

    链表

---
+++ link-list/list.go
@@ -0,0 +1,27 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	for current != nil {
+		// 全部删除完再移动到下一个元素
+		for current.Next != nil && current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+	return head
+}
+
+func main() {
+	head := &ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+	head.Next.Next.Next = &ListNode{Val: 3}
+	deleteDuplicates(head)
+}

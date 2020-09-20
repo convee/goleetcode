a5cfdebaf51287bd6ab5a334f796d369cbea91da
convee <convee.cn@gmail.com>
2020-09-20 22:52:36 +0800

    二进制链表转整数

---
+++ link-list/1290.convert-binary-number-in-a-linked-list-to-integer.go
@@ -0,0 +1,20 @@
+package main
+
+//1290. 二进制链表转整数
+//给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+
+//请你返回该链表所表示数字的 十进制值
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getDecimalValue(head *ListNode) int {
+	sum := 0
+	for head != nil {
+		sum = sum*2 + head.Val
+		head = head.Next
+	}
+	return sum
+}

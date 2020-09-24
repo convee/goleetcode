9fc3cfa5cc7a9931e330d87f7cc9ac50ad4a85fb
convee <convee.cn@gmail.com>
2020-09-24 22:21:49 +0800

    strstr

---
+++ string/28.implement-strstr.go
@@ -0,0 +1,22 @@
+package string
+
+//28. 实现 strStr()
+//给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+func strStr(haystack string, needle string) int {
+	if len(haystack) == 0 {
+		return 0
+	}
+	var i, j int
+	for i = 0; i < len(haystack)-len(needle)+1; i++ {
+		for j = 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+
+		}
+		if len(needle) == j {
+			return i
+		}
+	}
+	return -1
+}

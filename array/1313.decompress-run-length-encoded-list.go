24f2353f48a56bf7f79240d690f024509be5e7b2
convee <convee.cn@gmail.com>
2020-09-28 22:57:26 +0800

    解压缩编码列表

--- array/1313.decompress-run-length-encoded-list.go
+++ array/1313.decompress-run-length-encoded-list.go
@@ -20,3 +20,13 @@ func decompressRLElist(nums []int) []int {
 	}
 	return res
 }
+
+func decompressRLElist2(nums []int) []int {
+	var result []int
+	for i := 0; i < len(nums); i = i + 2 {
+		for j := 0; j < nums[i]; j++ {
+			result = append(result, nums[i+1])
+		}
+	}
+	return result
+}

2e0809b2ad072730d1e76863e8890c3c43f7a598
convee <convee.cn@gmail.com>
2020-09-30 23:32:29 +0800

    加一

--- array/66.plus-one.go
+++ array/66.plus-one.go
@@ -22,3 +22,16 @@ func plusOne(digits []int) []int {
 	}
 	return append([]int{1}, digits...)
 }
+
+func plusOne2(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	nums := make([]int, len(digits)+1)
+	nums[0] = 1
+	return nums
+}

80612f39407c523d2880ad9a8c4b66d9928edb76
convee <convee.cn@gmail.com>
2020-10-03 22:29:23 +0800

    整数反转

--- math/7.reverse-integer.go
+++ math/7.reverse-integer.go
@@ -51,6 +51,24 @@ func reverse(x int) int {
 	return num
 }
 
+func reverse2(x int) int {
+	rev := 0
+	INT_MIN := -2147483648
+	INT_MAX := 2147483647
+	for x != 0 {
+		pop := x % 10
+		if rev > INT_MAX/10 || (rev == INT_MAX/10 && pop > 7) {
+			return 0
+		}
+		if rev < INT_MIN/10 || (rev == INT_MIN/10 && pop < -8) {
+			return 0
+		}
+		x /= 10
+		rev = rev*10 + pop
+	}
+	return rev
+}
+
 // func main() {
 // 	fmt.Println(reverse(-123))
 // }

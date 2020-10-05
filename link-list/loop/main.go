e195e3b39a4a18cd28e2cd3eb509f92a8b5cc7d8
convee <convee.cn@gmail.com>
2020-10-05 22:15:54 +0800

    循环链表节点操作

--- link-list/loop/main.go
+++ link-list/loop/main.go
@@ -28,6 +28,42 @@ func New(n int) *Ring {
 	r.prev = p
 	return r
 }
+
+//获取下一个节点
+func (r *Ring) Next() *Ring {
+	if r.next == nil {
+		return r.init()
+	}
+	return r.next
+}
+
+//获取上一个节点
+func (r *Ring) Prev() *Ring {
+	if r.prev == nil {
+		return r.init()
+	}
+	return r.prev
+}
+
+//获取第n个节点
+func (r *Ring) Move(n int) *Ring {
+	if r.next == nil {
+		return r.init()
+	}
+	switch {
+	case n < 0:
+		for ; n < 0; n++ {
+			r = r.prev
+		}
+	case n > 0:
+
+		for ; n > 0; n-- {
+			r = r.next
+		}
+	}
+	return n
+
+}
 func main() {
 	r := new(Ring)
 	r.init()

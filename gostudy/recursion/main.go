b58936f66b01f8ac8553b36dfd0cbf45d498fe69
convee <convee.cn@gmail.com>
2020-10-09 23:01:05 +0800

    斐波那契数列

--- gostudy/recursion/main.go
+++ gostudy/recursion/main.go
@@ -19,7 +19,17 @@ func RescuvieTail(n int, a int) int {
 	return RescuvieTail(n-1, a*n)
 }
 
+//斐波那契数列：后一个数是前两个数的和的一种数列
+//1 1 2 3 5 8 13 21 ... N-1 N 2N-1
+func F(n int, a1, a2 int) int {
+	if n == 0 {
+		return a1
+	}
+	return F(n-1, a2, a1+a2)
+}
+
 func main() {
 	fmt.Println(Rescuvie(5))
 	fmt.Println(RescuvieTail(5, 1))
+	fmt.Println(F(5, 1, 1))
 }

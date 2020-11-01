b771642538017e93ee8788c60adc99fff3150992
convee <convee.cn@gmail.com>
2020-11-01 23:42:28 +0800

    剑指offer递归求和

---
+++ bitwise/offer.64.qiu-12n-lcof/main.go
@@ -0,0 +1,18 @@
+package main
+
+var res int
+
+// 剑指 Offer 64. 求1+2+…+n
+// 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+// https://leetcode-cn.com/problems/qiu-12n-lcof/
+// 思路：递归
+func sumNums(n int) int {
+	res = 0
+	sum(n)
+	return res
+}
+
+func sum(n int) bool {
+	res += n
+	return n > 0 && sum(n-1)
+}

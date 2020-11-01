091167ca98c5fbb9c4b3c345302b4a7fade1cf58
convee <convee.cn@gmail.com>
2020-11-02 00:11:30 +0800

    位运算

--- bitwise/1342.number-of-steps-to-reduce-a-number-to-zero/main.go
+++ bitwise/1342.number-of-steps-to-reduce-a-number-to-zero/main.go
@@ -1,7 +1,9 @@
 package main
 
-//1342. 将数字变成 0 的操作次数
-//https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+// 1342. 将数字变成 0 的操作次数
+// 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+// https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
 func numberOfSteps(num int) int {
 	n := 0
 	for {
--- bitwise/231.power-of-two/main.go
+++ bitwise/231.power-of-two/main.go
@@ -3,6 +3,11 @@ package main
 // 231. 2的幂
 // 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
 // https://leetcode-cn.com/problems/power-of-two/
+// 可以看到，对于N为2的幂的数，都有 N&(N-1)=0 ，所以这就是我们的判断条件。
+// 例如 2^3 = 8,
+// 8 的二进制 1 0 0 0
+// 7 的二进制 0 1 1 1
+// 7 & 8 = 0
 func isPowerOfTwo(n int) bool {
 	return n > 0 && n&(n-1) == 0
 }

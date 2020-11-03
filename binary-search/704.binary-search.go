66fb5fca8635b508b020a0c305e6d657a932ba05
convee <convee.cn@gmail.com>
2020-11-03 23:16:52 +0800

    二分查找

--- binary-search/704.binary-search.go
+++ binary-search/704.binary-search.go
@@ -1,7 +1,13 @@
 package main
 
-//704. 二分查找
-//https://leetcode-cn.com/problems/binary-search/
+// 704. 二分查找
+// https://leetcode-cn.com/problems/binary-search/
+
+// 总结一下一般实现的几个条件：
+// 初始条件：left = 0, right = length-1
+// 终止：left > right
+// 向左查找：right = mid-1
+// 向右查找：left = mid +1
 func search(nums []int, target int) int {
 	l, r, mid := 0, len(nums)-1, (len(nums)-1)/2
 	for r >= l {

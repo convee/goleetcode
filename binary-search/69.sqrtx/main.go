c4ecf29488c0fa33fe7cf2a784433fcdb459656b
convee <convee.cn@gmail.com>
2020-11-05 00:14:42 +0800

    x的平方根

--- binary-search/69.sqrtx/main.go
+++ binary-search/69.sqrtx/main.go
@@ -18,3 +18,19 @@ func mySqrt(x int) int {
 	}
 	return left
 }
+
+func mySqrt2(x int) int {
+	left := 0
+	right := x
+	for left <= right {
+		mid := (left + right) / 2
+		if mid*mid == x {
+			return mid
+		} else if mid*mid < x {
+			left = mid + 1
+		} else if mid*mid > x {
+			right = mid - 1
+		}
+	}
+	return right
+}

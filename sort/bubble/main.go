b38c75b7ac9806568bc7d46d8dca056521ef1129
convee <convee.cn@gmail.com>
2020-11-11 00:21:43 +0800

    冒泡排序

---
+++ sort/bubble/main.go
@@ -0,0 +1,13 @@
+package main
+
+func bubbleSort(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}

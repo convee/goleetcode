39636da5b4b690299a95e1f0d6e1cfe18d9a1b53
convee <convee.cn@gmail.com>
2020-11-11 23:23:16 +0800

    买卖股票的最佳时机

---
+++ dp/122.best-time-to-buy-and-sell-stock-ii/main.go
@@ -0,0 +1,14 @@
+package main
+
+//122. 买卖股票的最佳时机 II
+//https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+//思路：只要今天比昨天大，就卖出
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+	return maxProfit
+}

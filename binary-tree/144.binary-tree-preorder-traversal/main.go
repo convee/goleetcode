e62506e66568e7a5c906cb48473c9a325567a70a
convee <convee.cn@gmail.com>
2020-11-15 23:47:51 +0800

    二叉树前序遍历-迭代法

--- binary-tree/144.binary-tree-preorder-traversal/main.go
+++ binary-tree/144.binary-tree-preorder-traversal/main.go
@@ -6,8 +6,10 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-//144. 二叉树的前序遍历
-//https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+// 144. 二叉树的前序遍历
+// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+
+// 思路：递归
 func preorderTraversal(root *TreeNode) []int {
 	var res []int
 	res = preorder(root, res)
@@ -20,5 +22,23 @@ func preorder(root *TreeNode, res []int) []int {
 	res = append(res, root.Val)
 	res = preorder(root.Left, res)
 	res = preorder(root.Right, res)
+	return res
+}
+
+// 思路：迭代
+func preorderTraversal(root *TreeNode) []int {
+	var res []int
+	var stack []*TreeNode
+
+	for 0 < len(stack) || root != nil { //root != nil 只为了第一次root判断，必须放最后
+		for root != nil {
+			res = append(res, root.Val)       //前序输出
+			stack = append(stack, root.Right) //右节点 入栈
+			root = root.Left                  //移至最左
+		}
+		index := len(stack) - 1 //栈顶
+		root = stack[index]     //出栈
+		stack = stack[:index]
+	}
 	return res
 }

676dfa117e2f1b88e43a5b0eaeee8899fb4b5b35
convee <convee.cn@gmail.com>
2020-11-18 22:42:36 +0800

    对称二叉树

---
+++ binary-tree/101.symmetric-tree/main.go
@@ -0,0 +1,34 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 思路：递归
+// 1.两个树的跟节点相同
+// 2.每个树的左子树和另外一个树的右子树相同
+// 3.每个树的右子树和另外一个树的左子树相同
+func isSymmetric(root *TreeNode) bool {
+	return check(root, root)
+}
+
+func check(a, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	if a.Val != b.Val {
+		return false
+	}
+	if !check(a.Left, b.Right) {
+		return false
+	}
+	if !check(a.Right, b.Left) {
+		return false
+	}
+	return true
+}

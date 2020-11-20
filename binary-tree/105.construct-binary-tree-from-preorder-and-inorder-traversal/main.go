d37a760198f30501a02997c24fa067593433a600
convee <convee.cn@gmail.com>
2020-11-21 00:02:41 +0800

    从中序与后序遍历序列构造二叉树

--- binary-tree/105.construct-binary-tree-from-preorder-and-inorder-traversal/main.go
+++ binary-tree/105.construct-binary-tree-from-preorder-and-inorder-traversal/main.go
@@ -3,6 +3,13 @@ package main
 // 105. 从前序与中序遍历序列构造二叉树
 // https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 // 思路：递归
+// 前序遍历 preorder = [3,9,20,15,7]
+// 中序遍历 inorder = [9,3,15,20,7]
+//  3
+// / \
+//9  20
+//  /  \
+// 15   7
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -13,16 +20,21 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	if len(preorder) <= 0 || len(inorder) <= 0 {
 		return nil
 	}
-	rv := preorder[len(preorder)-1]
-	var i int
-	for i = range inorder {
-		if inorder[i] == rv {
+	// 中顺序列找根结点
+	var root int
+	for k, v := range inorder {
+		if v == preorder[0] {
+			root = k
 			break
 		}
 	}
+	// 左右子树归类
+	// pre_left, pre_right := preorder[1: root+1], preorder[root+1:]
+	// in_left, in_right := inorder[0: root], inorder[root+1:]
+	// 左右子树递归
 	return &TreeNode{
-		Val:   rv,
-		Left:  buildTree(preorder[:i], inorder[:i]),
-		Right: buildTree(preorder[i:len(preorder)-1], inorder[i+1:]),
+		Val:   preorder[0],
+		Left:  buildTree(preorder[1:root+1], inorder[0:root]),
+		Right: buildTree(preorder[root+1:], inorder[root+1:]),
 	}
 }

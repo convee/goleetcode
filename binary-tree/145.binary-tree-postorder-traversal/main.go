c5c2c3111bdf4cccfd50ff7dcfe1f1e78d6c1f5b
convee <convee.cn@gmail.com>
2020-11-23 23:29:07 +0800

    后序遍历-迭代法

--- binary-tree/145.binary-tree-postorder-traversal/main.go
+++ binary-tree/145.binary-tree-postorder-traversal/main.go
@@ -6,8 +6,9 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-//145. 二叉树的后序遍历
-//https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+// 145. 二叉树的后序遍历
+// https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+// 思路：递归
 func postorderTraversal(root *TreeNode) []int {
 	var res []int
 	return postorder(root, res)
@@ -21,4 +22,27 @@ func postorder(root *TreeNode, res []int) []int {
 	res = postorder(root.Right, res)
 	res = append(res, root.Val)
 	return res
+}
+
+// 思路：迭代
+func postorderTraversal(root *TreeNode) (res []int) {
+	stk := []*TreeNode{}
+	var prev *TreeNode
+	for root != nil || len(stk) > 0 {
+		for root != nil {
+			stk = append(stk, root)
+			root = root.Left
+		}
+		root = stk[len(stk)-1]
+		stk = stk[:len(stk)-1]
+		if root.Right == nil || root.Right == prev {
+			res = append(res, root.Val)
+			prev = root
+			root = nil
+		} else {
+			stk = append(stk, root)
+			root = root.Right
+		}
+	}
+	return
 }

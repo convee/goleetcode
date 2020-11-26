1b8434511c054fc2c8d1fcf3f365e5e43529bee4
convee <convee.cn@gmail.com>
2020-11-26 23:36:27 +0800

    平衡二叉树

---
+++ binary-tree/110.balanced-binary-tree/main.go
@@ -0,0 +1,40 @@
+package main
+
+// 110.平衡二叉树
+// https://leetcode-cn.com/problems/balanced-binary-tree/
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if !isBalanced(root.Left) || !isBalanced(root.Right) {
+		return false
+	}
+	leftH := maxDepth(root.Left) + 1
+	rightH := maxDepth(root.Right) + 1
+	if abs(leftH-rightH) > 1 {
+		return false
+	}
+	return true
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}

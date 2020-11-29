78f4b6b38fe443cd60ceb694e25fa5100f778524
convee <convee.cn@gmail.com>
2020-11-29 23:51:19 +0800

    快排（递归）

---
+++ gostudy/sort/quick/main.go
@@ -0,0 +1,14 @@
+package main
+
+// 快速排序
+func sort(nums []int, lo int, hi int) {
+	// 临界点 p
+	p := partition(nums, lo, hi)
+	sort(nums, lo, p-1)
+	sort(nums, hi, p+1)
+}
+
+// 交换位置
+func partition(nums []int, lo int, hi int) int {
+
+}

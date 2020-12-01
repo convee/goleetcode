e345bb5391c33e3e109a4859eab70c9b13dcc434
convee <convee.cn@gmail.com>
2020-12-01 23:06:55 +0800

    填充每个节点的下一个右侧节点指针

--- binary-tree/116.populating-next-right-pointers-in-each-node/main.go
+++ binary-tree/116.populating-next-right-pointers-in-each-node/main.go
@@ -37,3 +37,22 @@ func connect(root *Node) *Node {
 	}
 	return root
 }
+
+// 思路：递归
+func connect2(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	connectTwoNode(root.Left, root.Right)
+	return root
+}
+
+func connectTwoNode(node1 *Node, node2 *Node) {
+	if node1 == nil || node2 == nil {
+		return
+	}
+	node1.Next = node2
+	connectTwoNode(node1.Left, node1.Right)
+	connectTwoNode(node2.Left, node2.Right)
+	connectTwoNode(node1.Right, node2.Left)
+}

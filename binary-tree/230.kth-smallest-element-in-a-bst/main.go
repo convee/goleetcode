0ab21c5521de6f09faa143f45e3eba425d630bd9
convee <convee.cn@gmail.com>
2020-12-05 23:49:14 +0800

    二叉搜索树中第K小的元素

--- binary-tree/230.kth-smallest-element-in-a-bst/main.go
+++ binary-tree/230.kth-smallest-element-in-a-bst/main.go
@@ -2,6 +2,10 @@ package main
 
 // 230. 二叉搜索树中第K小的元素
 // https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+// 思路：
+// 1、对于 BST 的每一个节点node，左子树节点的值都比node的值要小，右子树节点的值都比node的值大。
+// 2、对于 BST 的每一个节点node，它的左侧子树和右侧子树都是 BST。
+// 3、BST 的中序遍历结果是有序的（升序）。
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode

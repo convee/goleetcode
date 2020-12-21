31647907759ab18e8cd12726781a6066cb97f899
convee <convee.cn@gmail.com>
2020-12-21 23:47:51 +0800

    环形链表-快慢指针

--- link-list/142.linked-list-cycle-ii/main.go
+++ link-list/142.linked-list-cycle-ii/main.go
@@ -18,3 +18,23 @@ func detectCycle(head *ListNode) *ListNode {
 	}
 	return nil
 }
+
+func detectCycle2(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil {
+		slow = slow.Next
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next
+		if fast == slow {
+			p := head
+			for p != slow {
+				p = p.Next
+				slow = slow.Next
+			}
+			return p
+		}
+	}
+	return nil
+}

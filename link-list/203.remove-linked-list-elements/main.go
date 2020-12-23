c09a54dacf67afe3275d0cd63f72b77e81ade530
convee <convee.cn@gmail.com>
2020-12-23 22:46:17 +0800

    移除链表元素

--- link-list/203.remove-linked-list-elements/main.go
+++ link-list/203.remove-linked-list-elements/main.go
@@ -8,18 +8,21 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 思路：双指针
 func removeElements(head *ListNode, val int) *ListNode {
+	// 定义哨兵结点
 	sentinel := &ListNode{0, head}
-	pre := sentinel
-
-	for head != nil {
-		if head.Val == val {
-			pre.Next = head.Next
+	pre := sentinel //前指针
+	cur := head     //当前指针
+	for cur != nil {
+		// 前指针移动
+		if cur.Val == val {
+			pre.Next = cur.Next
 		} else {
-			pre = head
+			pre = cur
 		}
-		head = head.Next
+		// 当前指针移动
+		cur = cur.Next
 	}
 	return sentinel.Next
-
 }

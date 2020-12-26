92748477ae4ddae1418a5f4b8bc04f03cab7e827
convee <convee.cn@gmail.com>
2020-12-26 23:25:22 +0800

    完全平方数

---
+++ queue/279.perfect-squares/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+// 279. 完全平方数
+// https://leetcode-cn.com/problems/perfect-squares/
+func numSquares(n int) int {
+	m := int(math.Sqrt(float64(n)))
+	dp := make([]int, n+1)
+	dp[0] = 0
+	for i := 1; i <= n; i++ {
+		dp[i] = math.MaxInt64
+	}
+	for i := 1; i <= m; i++ {
+		for j := i * i; j <= n; j++ {
+			dp[j] = min(dp[j], dp[j-i*i]+1)
+		}
+	}
+	return dp[n]
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	} else {
+		return x
+	}
+}
